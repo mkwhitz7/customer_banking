716aa1e81d7297d8dfd6fb82cfe89744986732b3
Michael Whitley <mkwhitz7@gmail.com>
2024-08-30 12:05:58 -0500

    completed files

--- savings_account.py
+++ savings_account.py
@@ -1,4 +1,5 @@
 # Account.py
+from Account import Account
 # Class Account:
 def __init__(self, balance, interest_rate):
         self.balance = balance
@@ -31,6 +32,7 @@ def create_savings_account(balance, interest, months):
         interest_rate (float): The APR interest rate for the savings account.
         months (int): The length of months to determine the amount of interest.
 
+        
     Returns:
         float: The updated savings account balance after adding the interest earned.
         And returns the interest earned.
@@ -38,7 +40,7 @@ def create_savings_account(balance, interest, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     savings_account = Account(balance, 0)
     # Calculate interest earned
-    interest = balance * (interest / 100) * (months / 12)
+    interest_earned = balance * (interest / 100) * (months / 12)
 
     # Update the savings account balance by adding the interest earned
     updated_balance = balance + interest
